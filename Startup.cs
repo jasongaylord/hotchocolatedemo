de5b11d2bf96f74355317151550db070f0c1fde4
Jason Gaylord <jason@jasongaylord.com>
2020-11-02 14:17:53 -0500

    Added the endpoint.

--- Startup.cs
+++ Startup.cs
@@ -35,10 +35,7 @@ namespace hotchocolatedemo
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapGraphQL();
             });
         }
     }
