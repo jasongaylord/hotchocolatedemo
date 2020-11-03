84d8333d6400ca05f563a229b525035a65fe3e69
Jason Gaylord <jason@jasongaylord.com>
2020-11-03 15:25:26 -0500

    Added query for manufacturers.

--- Graph/Query.cs
+++ Graph/Query.cs
@@ -6,6 +6,7 @@ namespace hotchocolatedemo.Graph
 {
     public class Query
     {
+        public IQueryable<Manufacturer> GetManufacturers([Service] ApplicationDbContext context) => context.Manufacturers;
         public IQueryable<Product> GetProducts([Service] ApplicationDbContext context) => context.Products;
     }
 }
