a792f3cefa8d6f48eb3ac510f81b7c2084478234
Jason Gaylord <jason@jasongaylord.com>
2020-11-03 13:41:53 -0500

    Updated inputs

---
+++ Graph/Inputs.cs
@@ -0,0 +1,9 @@
+using System;
+using hotchocolatedemo.Data;
+using HotChocolate.Types.Relay;
+
+namespace hotchocolatedemo.Graph
+{
+    public record AddManufacturerInput(string Name);
+    public record AddProductInput(string Name, float Price, DateTime LastUpdated, [ID(nameof(Manufacturer))] int ManufacturerId);
+}
